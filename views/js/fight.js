269ad4aecff168987d77acf61e8f685ff88fc033
Javier Estévez Villa <jestevez.villa@gmail.com>
2014-05-10 13:25:41 +0200

    url de las imágenes

--- views/js/fight.js
+++ views/js/fight.js
@@ -69,7 +69,7 @@ var Fight = {};
 			document.getElementById('ciudad').innerText = this.card.ciudad;
 			document.getElementById('habitantes').innerText = this.card.habitantes;
 			document.getElementById('renta').innerText = this.card.renta;
-			document.getElementById('imagen').src = 'img/imagen_' + this.card.cartodb_id;			
+			document.getElementById('imagen').src = '../img/imagen_' + this.card.cartodb_id;			
 
 			document.getElementById('accesibilidad').innerText = this.card.accesibilidad_y_movilidad_m_100_urbantur_2012;
 			document.getElementById('no2').innerText = this.card.concentracion_no2_mg_m3;
